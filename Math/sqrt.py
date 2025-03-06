bda86d24a8846f51c9b8e7e1e5242c6e7a19fdd1
Matteo Sanderson <matteosanderson@gmail.com>
2025-03-06 21:56:12 +0100

    changed square root calculation

--- Math/sqrt.py
+++ Math/sqrt.py
@@ -1,9 +1,8 @@
 import init
-from math import sqrt as root
 from LinAlg.matrix import ndarray
 
 def sqrt(x:ndarray):
     for j in range(len(x)):
         for i in range(len(x[0])):
-            x[j][i] = root(x[j][i])
+            x[j][i] = x[j][i]**(1/2)
     return x
