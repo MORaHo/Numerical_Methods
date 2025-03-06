2d238b9c03279d147a890c549ed8a03c40b898b8
Matteo Sanderson <matteosanderson@gmail.com>
2025-03-06 17:38:25 +0100

    legibility

--- Math/sqrt.py
+++ Math/sqrt.py
@@ -1,9 +1,9 @@
 import init
-from math import sqrt as sq
+from math import sqrt as root
 from LinAlg.matrix import ndarray
 
 def sqrt(x:ndarray):
     for j in range(len(x)):
         for i in range(len(x[0])):
-            x[j][i] = sq(x[j][i])
+            x[j][i] = root(x[j][i])
     return x
