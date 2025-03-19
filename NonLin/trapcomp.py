e2ad2da5ae7e43f0c3d07a33d03437a90be81745
Matteo Sanderson <matteosanderson@gmail.com>
2025-03-19 22:28:26 +0100

    added square and trapizoidal based integration

---
+++ NonLin/trapcomp.py
@@ -0,0 +1,11 @@
+import init
+from Misc.linspace import linspace
+from Maths.maths import cos
+
+def trapcomp(a,b,N:int,f):
+    
+    h = (b-a)/N
+    x = linspace(a+h,b-h,N)
+    I = h * (0.5*f(a) + f(x).sum() + 0.5*f(b))
+
+    return I
