698d7805f404a568a17ad8102b2a43e1782d4eaa
Matteo Sanderson <35639953+MORaHo@users.noreply.github.com>
2025-03-21 00:07:51 +0100

    Update trapcomp.py

--- NonLin/trapcomp.py
+++ NonLin/trapcomp.py
@@ -5,7 +5,7 @@ from Maths.maths import cos
 def trapcomp(a,b,N:int,f):
     
     h = (b-a)/N
-    x = linspace(a+h,b-h,N)
+    x = linspace(a+h,b-h,N-2)
     I = h * (0.5*f(a) + f(x).sum() + 0.5*f(b))
 
     return I
