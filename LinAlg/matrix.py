81aebe6fae605c145f1d940b05bf77590435cff6
MORaHo <matteosanderson@gmail.com>
2025-06-18 00:41:26 +0200

    added tuple-based single entry for __setitem__

--- LinAlg/matrix.py
+++ LinAlg/matrix.py
@@ -74,45 +74,71 @@ class ndarray():
                     return Matrix(M)
 
                 return self.matrix[y][x]
+        
         except:
             return []
         
     def __setitem__(self,index,item):
 
-        if type(index)==tuple and len(index)==2 and (type(item) == Matrix or type(item) == Vector):
-                y,x = index
-                if type(item) == Matrix:
-                    if type(y) == slice and (type(x) == slice or type(x) == int):
-                        
-                        if type(x) == slice:
-
-                            y_start = int(0 if y.start == None else y.start)
-                            y_end = int(len(self.matrix) if y.stop == None else y.stop)
-                            x_start = int(0 if x.start == None else x.start)
-                            x_end = int(len(self.matrix[0]) if x.stop == None else x.stop)
-                            
-                            for j in range(y_start,y_end):
-                                for i in range(x_start,x_end):
-                                    self.matrix[j][i] = item[j-y_start][i-x_start]
-                        
-                        if type(x) == int:
-                            
-                            y_start = int(0 if y.start == None else y.start)
-                            y_end = int(len(self.matrix) if y.stop == None else y.stop)
-                            
-                            for j in range(y_start,y_end):
-                                    self.matrix[j][x] = item[j-y_start][x]
+        if type(index)==tuple and len(index)==2:
                 
-                if type(item) == Vector:
+            y,x = index
 
-                    y_start = int(0 if y.start == None else y.start)
-                    y_end = int(len(self.matrix) if y.stop == None else y.stop)
+            if type(item) == Matrix:
+
+                i_n,i_m = item.size()
+    
+                if type(y) == slice and (type(x) == slice or type(x) == int):
                     
-                    for j in range(y_start,y_end):
-                            self.matrix[j][x] = item[j-y_start][0]
-                
+                    if type(x) == slice:
 
-                return self.matrix[y][x]
+
+                        y_start = int(0 if y.start == None else y.start)
+                        y_end = int(len(self.matrix) if y.stop == None else y.stop)
+                        x_start = int(0 if x.start == None else x.start)
+                        x_end = int(len(self.matrix[0]) if x.stop == None else x.stop)
+
+                        if i_n*i_m != (y_end-y_start+1)*(x_end-x_start+1):
+                            print("Item change dimensions do not match, check the what you are trying to change")
+                            sys.exit()
+
+                        for j in range(y_start,y_end):
+                            for i in range(x_start,x_end):
+                                self.matrix[j][i] = item[j-y_start][i-x_start]
+
+                    
+                    if type(x) == int:
+                        
+                        y_start = int(0 if y.start == None else y.start)
+                        y_end = int(len(self.matrix) if y.stop == None else y.stop)
+
+                        if i_n != (y_end-y_start+1):
+                            print("Item change dimensions do not match, check the what you are trying to change")
+                            sys.exit()
+                        
+                        for j in range(y_start,y_end):
+                                self.matrix[j][x] = item[j-y_start][x]
+
+            elif type(item) == Vector:
+                
+                i_n = len(item)
+                y_start = int(0 if y.start == None else y.start)
+                y_end = int(len(self.matrix) if y.stop == None else y.stop)
+
+                if i_n != (y_end-y_start+1):
+                    print("Item change dimensions do not match, check the what you are trying to change")
+                    sys.exit()
+                
+                for j in range(y_start,y_end):
+                        self.matrix[j][x] = item[j-y_start][0]
+
+            elif type(item) in numbers.__args__:
+                
+                if type(x) != int or type(y) != int:
+                    print("Input size does not match")
+                    sys.exit()
+                
+                self.matrix[y][x] = item
         
         else:
             self.matrix[index] = item  
