a4cbdb32769a48b05fd5c7bcb64bdeac46fece23
MORaHo <matteosanderson@gmail.com>
2025-06-23 00:33:55 +0200

    fixed backward substitution

--- LinAlg/solve.py
+++ LinAlg/solve.py
@@ -20,8 +20,7 @@ def forward_substitution(L:Matrix,b:Vector):
         for i in range(j):
             b_j -= L[j][i]*y[i]
         y.append(b_j/L[j][j])
-    
-    return Matrix(y)
+    return Vector(y)
 
 fwd_sub = forward_substitution
 
@@ -29,15 +28,14 @@ def backward_substitution(U:Matrix,y:Vector):
     y = y.col()
     n = len(y)-1
     x = zeros(n+1,1)
-    x[n][0] = y[n][0]/U[n][n]
+    x[n][0] = y[-1][0]/U[-1][-1]
     
     for j in range(n-1,-1,-1):
         y_j = y[j][0]
         for i in range(n,j,-1):
             y_j -= U[j][i]*x[i][0]
         x[j][0] = y_j/U[j][j]
-
-    return Matrix(x)
+    return x
 
 bkw_sub = backward_substitution
 
@@ -50,18 +48,20 @@ def solve(A:Matrix,b:Vector):
 
     if det(A) == 0:
         print("Solution may not be unique!")
+
+    #this will be expanded to handle more and more cases, initially I intend to add a QR solver and in the end for this function to basically become the \ of matlab
     
     # Following diagram in the algorithm section of https://uk.mathworks.com/help/matlab/ref/double.mldivide.html
 
-    if m != n: #if rectangular
-        [Q,R] = qr(A) #apply qr
+    if m != n:
+        [Q,R] = qr(A)
         Q1 = Q[:m,:n]
-        R1 = R[:n,:n] # remove rows which are not filled in
+        R1 = R[:n,:n]
         y = Q1.T()*b.col()
         x = bkw_sub(R1,y)
-    elif isequal(tril(A),A): #lower triangular matrix can easily be solved
+    elif isequal(tril(A),A):
         x = fwd_sub(A,b)
-    elif isequal(triu(A),A): # upper triangular matrix can easily be solved
+    elif isequal(triu(A),A):
         x = bkw_sub(A,y)
     elif m <= 16:
         [L,U,P] = lu(A)
@@ -70,7 +70,8 @@ def solve(A:Matrix,b:Vector):
     else:
         if isequal((triu(A) + diag(diag(A,-1),-1)),A): #is the matrix upper-hessenberg
             if isequal((tril(A) + diag(diag(A,1),1)),A): #If matrix is tridiagonal
-                x = thomas(A,b) #apply thomas algorithm
+                #apply Thomas algorithm
+                x = thomas(A,b)
             else:
                 [U,Q] = triag(A) #decompose Hessenberg matrix into upper-triangular and Q (through givens rotations), which allows use to solve system
                 y = Q.T()*b.col() #Q.T() = inv(Q)
@@ -85,11 +86,4 @@ def solve(A:Matrix,b:Vector):
                 [L,U,P] = lu(A)
                 y = fwd_sub(L,P*b)
                 x = bkw_sub(U,y)
-    print(x.matrix)
     return x
-
-A = Matrix([[2,1,0,0,1,0],[1,2,1,0,0,1],[0,1,2,1,0,0],[0,0,1,2,1,0],[0,0,0,1,2,1],[0,0,0,0,1,2]])
-b = Vector([1,1,1,1,1,1])
-x = solve(A,b)
-print(x)
-print(A*x-b)
