ce072e3b28042315a15090d721f7f5b3c343875d
MORaHo <matteosanderson@gmail.com>
2025-06-24 01:04:39 +0200

    added 3 more tests

--- Tests/basics.py
+++ Tests/basics.py
@@ -5,6 +5,8 @@ from LinAlg.matrix import Matrix,Vector,ndarray
 
 ### This file implements tests to see if all the functionalities in LinAlg.matrix function or not, since without them nothing else will work
 
+## Matrix generation test
+
 matrix = [[1,1,1],[1,1,1],[1,1,1]]
 A = Matrix([[1,1,1],[1,1,1],[1,1,1]])
 A_ = Matrix([1,1,1,1,1,1,1,1,1],3,3)
@@ -14,7 +16,28 @@ if A.matrix != matrix or A_.matrix != matrix:
     print("Major error: Matrix generation not working!")
     sys.exit()
 
+x = Vector([1,1,1,1,1])
+vector_matrix = [[1],[1],[1],[1],[1]]
+
+if x.matrix != vector_matrix:
+    print("Vector not generating properly")
+    sys.exit()
+
 tests_passed += 1
+
+## Type-setting test
+
+# These test if the code properly generates a Vector in the special cases we have called upon the matrix generation
+x = Matrix([1,1,1,1,1])
+vector_matrix = [[1],[1],[1],[1],[1]]
+row = Matrix([[1,1,1,1,1]])
+row_matrix = [[1,1,1,1,1]]
+
+if x.matrix != vector_matrix or row.matrix != row_matrix:
+    print("Vector not generating properly from Matrix call")
+    sys.exit()
+
+## __setitem__ test
 
 A1 = Matrix([[1,1,1],[1,1,1],[1,1,1]])
 B = Vector([2,2])
@@ -31,3 +54,12 @@ if not isequal(A1,C1) or not isequal(A2,C2):
     sys.exit()
 
 tests_passed += 1
+
+## Transpose test
+
+A = Matrix([[1,2,3],[4,5,6],[7,8,9]])
+T = Matrix([[1,4,7],[2,5,8],[3,6,9]])
+
+if not isequal(A.T(),T):
+    print("Transpose matrix generation not working!")
+    sys.exit()
