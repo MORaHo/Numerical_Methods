04f8b677dacb1f1d775711062df71e3de4016e57
MORaHo <matteosanderson@gmail.com>
2025-11-22 21:09:31 +0100

    added chebyshev node generation

---
+++ src/Misc/chebyshev.py
@@ -0,0 +1,17 @@
+from src.LinAlg.matrix import Vector
+from src.Maths import cos
+from math import pi
+
+# This function generates a list of nodes generated according to Chebyshev-Gauss-Lobatto
+# N is the number of sectors between the nodes, not the number of nodes itself.
+
+def chebyshev(start,end,N:int):
+    nodes = []
+
+    for i in range(N+1):
+
+        x_star = -cos(pi*i/N)
+        x = (end+start)/2 + (end-start)*x_star/2
+        nodes.append(x)
+
+    return Vector(nodes)
