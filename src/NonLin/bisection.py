ce548c2fff69e3fc29268ff8ad94fa00c461cd8f
MORaHo <matteosanderson@gmail.com>
2025-11-30 23:14:58 +0100

    optimization of bisection

--- src/NonLin/bisection.py
+++ src/NonLin/bisection.py
@@ -16,21 +16,13 @@ def bisection(a,b,f:Callable):
         print("Start and end values with this function iare not valid for application of bisection method, either change limit values or function")
         sys.exit()
 
-    ORDER = f(a)>0 #this tells us which to assign c to, since depending on which of a and b is positive, who is assigned c is swapped.
     c = (b+a)/2
 
     while abs(f(c)) > EPS:
-        if f(c) > 0:
-            if ORDER:
-                a = c
-            else:
-                b = c
-        else:
-            if ORDER:
-                b = c
-            else:
-                a = c
-
+        if f(a)*f(c) < 0:
+            b = c
+        elif f(b)*f(c) < 0:
+            a = c
         c = (b+a)/2
 
     return c
