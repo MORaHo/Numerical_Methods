1116cd0a7f243d102abbb0c3ba16daa90ae73942
MORaHo <matteosanderson@gmail.com>
2025-12-11 00:24:52 +0100

    added toll to bisection inputs

--- src/NonLin/bisection.py
+++ src/NonLin/bisection.py
@@ -3,7 +3,7 @@ from collections.abc import Callable
 
 EPS = 1E-6
 
-def bisection(a,b,f:Callable):
+def bisection(a,b,f:Callable,toll=EPS):
     
     # checks for obvious issues
     if f(a) == 0:
@@ -18,7 +18,7 @@ def bisection(a,b,f:Callable):
 
     c = (b+a)/2
 
-    while abs(f(c)) > EPS:
+    while abs(f(c)) > toll:
         if f(a)*f(c) < 0:
             b = c
         elif f(b)*f(c) < 0:
